616e01e93defe7756a2f0dcc317deed1ffe2438a
MircoDev3110 <mirco.wrk@gmail.com>
2023-12-23 12:22:24 +0100

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+import './App.css';
+
+const App : React.FC = () => {
+
+
+  return(
+    <div className='form-wrap'>
+      <div className='centered-form'>
+
+        <h1 className='form-title'>Modulo Creazione Materiale</h1>
+        <p className='form-desc'>Work in progress</p>
+
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
